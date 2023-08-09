8a3260caa2701ef8d3ff93898914ae2026cca317
Alejandro <dupuyalejandro04@gmail.com>
2023-08-09 13:20:10 +1200

    estructura de carpetas

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express =  require(`express`);
+const path = require(`path`);
+const app = express();
+
+app.use(express.static(path.join(__dirname , `../public`)));
+
+const PORT = 3002;
+
+app.listen(PORT , ()=> {
+    console.log(`Servidor Funcionando`);
+})
+
+app.get(`/` , (req , res) => {
+    res.sendFile (path.join(__dirname , `./view/index.html`));
+})
