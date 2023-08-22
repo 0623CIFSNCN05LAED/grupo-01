e56c50522c3cba2cd7f5624e42543b20f8f68b22
Simon Orozco Henao <saimon2989@gmail.com>
2023-08-22 01:50:30 -0500

    Agregando marings a h3 y organizando estilos de la vitrina 2 del home

--- public/js/index.js
+++ public/js/index.js
@@ -18,4 +18,24 @@ document.addEventListener("DOMContentLoaded", function() {
 
     nextButton.addEventListener('click', nextSlide);
 });
+/* Slider function */
+// Función para avanzar al siguiente slide
+document.addEventListener("DOMContentLoaded", function() {
+    const slider = document.querySelector('.slider-wrapper2');
+    const slideWidth = document.querySelector('.section-product2').offsetWidth;
+    const nextButton = document.getElementById('nextButton2');
+
+    let currentIndex = 0;
+
+    function nextSlide() {
+        currentIndex++;
+        if (currentIndex >= slider.children.length) {
+        currentIndex = 0;
+        }
+      const transformValue = -currentIndex * slideWidth;
+        slider.style.transform = `translateX(${transformValue}px)`;
+    }
+
+    nextButton.addEventListener('click', nextSlide);
+});
 console.log('hola')
