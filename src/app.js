afc9af5b7801a94ae292f6587bb52e24d79c20e3
luispaca <luispaca@uca.edu.ar>
2023-08-22 21:56:30 -0300

    agregando cart-shopping.html & css

--- src/app.js
+++ src/app.js
@@ -3,17 +3,24 @@ const path = require(`path`);
 const app = express();
 
 app.use(express.static(path.join(__dirname , `../public`)));
-
 const PORT = 3002;
 
 app.listen(PORT , ()=> {
     console.log(`Servidor Funcionando en el puerto ${PORT}`);
 })
 
-app.get(`/` , (req , res) => {
-    res.sendFile (path.join(__dirname , `./view/index.html`));
+app.get('/' , (req , res) => {
+    res.sendFile (path.join(__dirname , './view/index.html'));
 });
-
-app.get(`/login`, (req , res) => {
-    res.sendFile (path.join(__dirname, `./view/login.html`))
+app.get('/registro', (req , res) => {
+    res.sendFile (path.join(__dirname, './view/registro.html'))
+});
+app.get('/login', (req , res) => {
+    res.sendFile (path.join(__dirname, './view/login.html'))
+});
+app.get('/cart-shopping', (req , res) => {
+    res.sendFile (path.join(__dirname, './view/cart-shopping.html'))
+});
+app.get('/detalle-producto', (req , res) => {
+    res.sendFile (path.join(__dirname, './view/details-product.html'))
 });
