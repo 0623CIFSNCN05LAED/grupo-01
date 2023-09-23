1c729988854bd373fe5a203ac4cd5409b25fa450
Alejandro <dupuyalejandro04@gmail.com>
2023-09-23 13:54:38 +1200

    cambios

--- src/routers/users.js
+++ src/routers/users.js
@@ -7,18 +7,18 @@ const router = express.Router();
 const userController = require("../controllers/user-controller");
 
 // *************** Get All Users ***************
-router.get("/",userController.index);
+router.get("/", userController.index);
 
 // ************ User Registration Form ************
-router.get("/register",userController.register);
+router.get("/register", userController.register);
 
 // **************** User Login Form ****************
-router.get("/login",userController.login);
+router.get("/login", userController.login);
 
 // ************** User Profile Form ****************
-router.get("/profile/:id",userController.profileUser);
+router.get("/profile/:id", userController.profileUser);
 
 // ************** Delete one user ******************
-router.delete("/:id",userController.deleteUser);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
