4a6c40e729098dfbff920bd495caab831f12a592
Alejandro <dupuyalejandro04@gmail.com>
2023-09-27 13:41:12 +1300

    validacion de login andando

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,12 +1,15 @@
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
+const { json } = require("express");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
+const { type } = require("os");
 
 const controller = {
-    index: (req,res) =>{
-        res.render();
-    },
-    register: (req,res) =>{
-        // Extract user information from the form
+  index: (req, res) => {
+    res.render();
+  },
+  register: (req, res) => {
+    // Extract user information from the form
     const name = req.body.nombreapellido;
     const phone = req.body.telefono;
     const email = req.body.email;
@@ -15,25 +18,48 @@ const controller = {
 
     // Save user information in a JSON file
     if (password === repassword) {
-      const user = {id: uuidv4(), name, email, password, phone };
-      fs.appendFile("src/data/users/users.json", JSON.stringify(user, null, 2) + "\n" ,(err) => {
-      if (err) throw err;
-      console.log('The file has been saved.');
-      });
-      res.send(user);
+      const user = { id: uuidv4(), name, email, password, phone };
+      fs.appendFile(
+        "src/data/users/users.json",
+        JSON.stringify(user, null, 2) + "\n",
+        (err) => {
+          if (err) throw err;
+          console.log("The file has been saved.");
+        }
+      );
+      res.redirect("/");
     } else {
       res.redirect("/register");
-    };
-  },
-    login: (req,res) =>{
-        res.render();
-    },
-    profileUser: (req,res) =>{
-        res.render();
-    },
-    deleteUser: (req,res) =>{
-        res.render();
     }
+  },
+  login: (req, res) => {
+    // usando datos de users.json
+    const users = JSON.parse(
+      fs.readFileSync("src/data/users/users.json", "utf-8")
+    );
+    console.log(users);
+    // datos del login
+    const email = req.body.email;
+    const password = req.body.password;
+    console.log(typeof email);
+    console.log(typeof password);
+    // comprobando que el email exista
+    const user = users.find((user) => user.email === email);
+    // chequeando password
+    if (user && user.password === password) {
+      res.redirect("/");
+      console.log("login exitoso");
+    } else {
+      res.redirect("/login");
+      console.log("algo mal");
+    }
+  },
+  profileUser: (req, res) => {
+    res.render();
+  },
+  deleteUser: (req, res) => {
+    res.render();
+  },
 };
 
 module.exports = controller;
--- src/routers/main.js
+++ src/routers/main.js
@@ -9,10 +9,11 @@ const userController = require("../controllers/user-controller");
 router.get("/", mainController.index);
 
 router.get("/login", mainController.login);
+router.post("/login", userController.login);
 router.get("/cart", mainController.cart);
 router.get("/details-product/:id", mainController.detailsproduct);
 router.get("/register", mainController.register);
-router.post("/register" , userController.register)
+router.post("/register", userController.register);
 
 const productRouter = require("./products");
 router.use("/products", productRouter);
