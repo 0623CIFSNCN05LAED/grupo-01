8b78a352a018c1fee0d6f3d1fd02763bd1e0981a
Danirodri0201 <Danirodri0201>
2023-12-12 23:30:14 -0300

    creando dashboard

---
+++ dashboard/src/components/Dashboard.jsx
@@ -0,0 +1,9 @@
+import './Dashboard.css'
+import SearchWrape from './Search-wrap'
+
+function Dashboard(){
+    return <div className="dashboard">
+        <SearchWrape/>
+    </div>
+}
+export default Dashboard
