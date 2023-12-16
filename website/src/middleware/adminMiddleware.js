67cb6d937849ec875e54416152a06aa9f94836c7
Simon Orozco Henao <saimon2989@gmail.com>
2023-12-16 10:13:52 -0500

    Corrigiendo AdminMiddelware junto con algunos estilos en los inputs

--- website/src/middleware/adminMiddleware.js
+++ website/src/middleware/adminMiddleware.js
@@ -1,6 +1,16 @@
-function adminMiddleware(req, res, next) {
+/* function adminMiddleware(req, res, next) {
   if (req.session.usuario.user_type_id != 1) {
     res.redirect("/users/user-profile/" + req.session.usuario.id);
+  }
+}
+
+module.exports = adminMiddleware; */
+
+function adminMiddleware(req, res, next) {
+  if (req.session.usuario && req.session.usuario.user_type_id != 1) {
+    res.redirect("/users/user-profile/" + req.session.usuario.id);
+  } else {
+    next();
   }
 }
 
