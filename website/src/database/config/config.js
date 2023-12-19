31980995715aa2cc17bc16dc13dffddcff776cee
luispaca <luispaca@uca.edu.ar>
2023-12-19 20:04:53 -0300

    user-profileValidation

--- website/src/database/config/config.js
+++ website/src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: "root",
     password: null,
-    database: "maunganui_db2",
+    database: "maunganui_db",
     host: "127.0.0.1",
     dialect: "mysql",
   },
