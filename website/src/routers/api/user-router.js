2e1eae3b63420f1dd17d4b5906c0e9fb6f2f9eb6
Alejandro <dupuyalejandro04@gmail.com>
2023-12-19 14:49:41 +1300

    agregando menu admin

--- website/src/routers/api/user-router.js
+++ website/src/routers/api/user-router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const apiUserController = require("../../controllers/api/userController");
 
-router.get("/api/users", apiUserController.list);
-router.get("/api/users/:id", apiUserController.show);
+router.get("/apiUsers", apiUserController.list);
+router.get("/apiUsers/:id", apiUserController.show);
 
 module.exports = router;
