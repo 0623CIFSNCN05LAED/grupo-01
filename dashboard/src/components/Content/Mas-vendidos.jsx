19fb8c9facf6903dc327e77e8928e3fcb27e6ee7
Alejandro <dupuyalejandro04@gmail.com>
2023-12-28 10:41:33 +1300

    change structure all dashboard

--- dashboard/src/components/Content/Mas-vendidos.jsx
+++ dashboard/src/components/Content/Mas-vendidos.jsx
@@ -1,95 +1,94 @@
 import camisabeige from "../../assets/img/products/camisa-beige.jpg";
 import yellowjacket from "../../assets/img/products/yellow-jacket.jpg";
 import blacktshirt from "../../assets/img/products/blackt-shirt.jpg";
-import amsterdamtop from "../../assets/img/products/amsterdam-top.jpg"
+import amsterdamtop from "../../assets/img/products/amsterdam-top.jpg";
 function MasVendidos() {
-    return (
-        <section className="content">
-        <h2>Más populares</h2>
-        <article className="person-boxes">
-          <div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
-            <div className="box-avatar">
-              <img src={camisabeige} alt="camisabeige" />
-            </div>
-            <div className="box-bio">
-              <h2 className="bio-name">Camisa Beige</h2>
-              <p className="bio-position">$35000</p>
-            </div>
-            <div className="box-actions">
-              <button>
-                <i className="bi bi-star"></i>
-              </button>
-              <button>
-                <i className="bi bi-chat"></i>
-              </button>
-              <button>
-                <i className="bi bi-envelope"></i>
-              </button>
-            </div>
+  return (
+    <section className="content">
+      <h2>Más Vendidos</h2>
+      <article className="person-boxes">
+        <div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
+          <div className="box-avatar">
+            <img src={camisabeige} alt="camisabeige" />
           </div>
-          <div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
-            <div className="box-avatar">
-              <img src={yellowjacket} alt="yellowjacket" />
-            </div>
-            <div className="box-bio">
-              <h2 className="bio-name">Yellow Jacket</h2>
-              <p className="bio-position">$85000</p>
-            </div>
-            <div className="box-actions">
-              <button>
-                <i className="bi bi-star"></i>
-              </button>
-              <button>
-                <i className="bi bi-chat"></i>
-              </button>
-              <button>
-                <i className="bi bi-envelope"></i>
-              </button>
-            </div>
+          <div className="box-bio">
+            <h2 className="bio-name">Camisa Beige</h2>
+            <p className="bio-position">$35000</p>
           </div>
-          <div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
-            <div className="box-avatar">
-              <img src={blacktshirt} alt="blacktshirt" />
-            </div>
-            <div className="box-bio">
-              <h2 className="bio-name">Black T-shirt</h2>
-              <p className="bio-position">$21000</p>
-            </div>
-            <div className="box-actions">
-              <button>
-                <i className="bi bi-star"></i>
-              </button>
-              <button>
-                <i className="bi bi-chat"></i>
-              </button>
-              <button>
-                <i className="bi bi-envelope"></i>
-              </button>
-            </div>
+          <div className="box-actions">
+            <button>
+              <i className="bi bi-star"></i>
+            </button>
+            <button>
+              <i className="bi bi-chat"></i>
+            </button>
+            <button>
+              <i className="bi bi-envelope"></i>
+            </button>
           </div>
-          <div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
-            <div className="box-avatar">
-              <img src={amsterdamtop} alt="amsterdamtop" />
-            </div>
-            <div className="box-bio">
-              <h2 className="bio-name">Top amsterdam</h2>
-              <p className="bio-position">$15000</p>
-            </div>
-            <div className="box-actions">
-              <button>
-                <i className="bi bi-star"></i>
-              </button>
-              <button>
-                <i className="bi bi-chat"></i>
-              </button>
-              <button>
-                <i className="bi bi-envelope"></i>
-              </button>
-            </div>
+        </div>
+        <div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
+          <div className="box-avatar">
+            <img src={yellowjacket} alt="yellowjacket" />
           </div>
-         
-        </article>
-      </section>
-    )
+          <div className="box-bio">
+            <h2 className="bio-name">Yellow Jacket</h2>
+            <p className="bio-position">$85000</p>
+          </div>
+          <div className="box-actions">
+            <button>
+              <i className="bi bi-star"></i>
+            </button>
+            <button>
+              <i className="bi bi-chat"></i>
+            </button>
+            <button>
+              <i className="bi bi-envelope"></i>
+            </button>
+          </div>
+        </div>
+        <div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
+          <div className="box-avatar">
+            <img src={blacktshirt} alt="blacktshirt" />
+          </div>
+          <div className="box-bio">
+            <h2 className="bio-name">Black T-shirt</h2>
+            <p className="bio-position">$21000</p>
+          </div>
+          <div className="box-actions">
+            <button>
+              <i className="bi bi-star"></i>
+            </button>
+            <button>
+              <i className="bi bi-chat"></i>
+            </button>
+            <button>
+              <i className="bi bi-envelope"></i>
+            </button>
+          </div>
+        </div>
+        <div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
+          <div className="box-avatar">
+            <img src={amsterdamtop} alt="amsterdamtop" />
+          </div>
+          <div className="box-bio">
+            <h2 className="bio-name">Top amsterdam</h2>
+            <p className="bio-position">$15000</p>
+          </div>
+          <div className="box-actions">
+            <button>
+              <i className="bi bi-star"></i>
+            </button>
+            <button>
+              <i className="bi bi-chat"></i>
+            </button>
+            <button>
+              <i className="bi bi-envelope"></i>
+            </button>
+          </div>
+        </div>
+      </article>
+    </section>
+  );
 }
 export default MasVendidos;
--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -3,12 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
